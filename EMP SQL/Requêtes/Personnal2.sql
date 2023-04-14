9a04763a483a43fe68c65d51f4293e2c262f8c3f
Suzuyara <118835164+L1k3Suzuya@users.noreply.github.com>
2023-04-14 14:15:22 +0200

    Add files via upload

--- EMP SQL/Requêtes/Personnal2.sql
+++ EMP SQL/Requêtes/Personnal2.sql
@@ -1,4 +1,105 @@
 -------------------------------------------------------------
+Partie 1 (EMP-SQL)
+-------------------------------------------------------------
+(1) Rechercher le prénom des employés et le numéro de la région de leur département.
+
+SELECT employe.prenom, dept.noregion 
+FROM employe 
+JOIN dept ON employe.nodep = dept.nodept
+
+-------------------------------------------------------------
+(2) Rechercher le numéro du département, le nom du département, le nom des employés classés par numéro de département (renommer les tables utilisées).
+
+SELECT dept.nom as departement, prenom, employe.nom, dept.nodept as departements 
+FROM employe 
+JOIN dept ON dept.nodept = employe.nodep 
+ORDER BY departements
+
+-------------------------------------------------------------
+(3) Rechercher le nom des employés du département Distribution.
+
+SELECT employe.nom ,dept.nom 
+FROM employe 
+JOIN dept ON employe.nodep = dept.nodept 
+WHERE dept.nom='distribution'
+
+-------------------------------------------------------------
+(4) Rechercher le nom et le salaire des employés qui gagnent plus que leur patron, et le nom et le salaire de leur patron.
+
+SELECT p.nom, p.salaire, psup.nom AS 'nom_sup', psup.salaire AS salaire_sup
+FROM employe as psup LEFT 
+OUTER JOIN employe AS p ON p.nosup = psup.noemp 
+WHERE p.salaire > psup.salaire
+
+-------------------------------------------------------------
+(5) Rechercher le nom et le titre des employés qui ont le même titre que Amartakaldire.
+
+SELECT nom, titre 
+FROM employe 
+WHERE titre IN (SELECT titre 
+FROM employe 
+WHERE nom = 'Amartakaldire')
+
+-------------------------------------------------------------
+(6) Rechercher le nom, le salaire et le numéro de département des employés qui gagnent plus un seul employé du département 31, classés par numéro de département et salaire.
+
+SELECT nom, salaire, nodep 
+FROM employe 
+WHERE salaire > ANY (SELECT salaire 
+FROM employe 
+WHERE nodep = 31) 
+ORDER BY nodep, salaire ASC
+
+-------------------------------------------------------------
+(7) Rechercher le nom, le salaire et le numéro de département des employés qui gagnent plus que tous les employés du département 31, classés par numéro de département et salaire
+
+SELECT nom, salaire, nodep 
+FROM employe 
+WHERE salaire > ALL (SELECT salaire 
+FROM employe 
+WHERE nodep = 31) 
+ORDER BY nodep, salaire ASC
+
+-------------------------------------------------------------
+(8) Rechercher le nom et le titre des employés du service 31 qui ont un titre que on trouve dans le département 32.
+
+SELECT nom, titre 
+FROM employe 
+WHERE titre IN (SELECT titre 
+FROM employe 
+WHERE nodep = 32) AND nodep = 31
+
+-------------------------------------------------------------
+(9) Rechercher le nom et le titre des employés du service 31 qui ont un titre on ne trouve pas dans le département 32.
+
+SELECT nom, titre 
+FROM employe 
+WHERE titre NOT IN (SELECT titre 
+FROM employe 
+WHERE nodep = 32) AND nodep = 31
+
+-------------------------------------------------------------
+(10) Rechercher le nom, le titre et le salaire des employés qui ont le même titre et le même salaire que Fairent.
+
+SELECT nom, titre, salaire 
+FROM employe 
+WHERE titre IN (SELECT titre 
+FROM employe 
+WHERE nom = 'Fairent') AND salaire IN (SELECT salaire 
+FROM employe 
+WHERE nom = 'Fairent')
+
+-------------------------------------------------------------
+(11) Rechercher le numéro de département, le nom du département, le nom des employés, en affichant aussi les départements dans lesquels il y a personne, classés par numéro de département.
+
+SELECT employe.nodep, dept.nom, employe.nom 
+FROM employe LEFT 
+JOIN dept ON employe.nodep = dept.nodept 
+ORDER BY employe.nodep
+
+-------------------------------------------------------------
+Partie 2 (EMP-SQL)
+-------------------------------------------------------------
 1. Calculer le nombre employés de chaque titre.
 
 SELECT titre, COUNT(*) AS nb_employes 
